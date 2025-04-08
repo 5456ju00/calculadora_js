815e5d2ba2154374e0f8f61d67c2cd5ea4ae177c
Júlia <160253571+5456ju00@users.noreply.github.com>
2025-04-08 09:09:52 -0300

    Estrutura básica de function.

---
+++ calculadora.js
@@ -0,0 +1,29 @@
+function soma(num1, num2) {
+    alert(num1 + num2);
+}
+
+function subtracao(num1, num2) {
+    alert(num1 - num2);
+}
+
+function multiplicacao(num1, num2) {
+    alert(num1 * num2);
+}
+
+function divisao(num1, num2) {
+    alert(num1 / num2);
+}
+
+alert ("Calculadora");
+
+let primeiroNumero = Number(promt("Digite o primeiro número!"));
+let segundoNumero = Number(promt("Digite o segundo número!"));
+
+soma(primeiroNumero, segundoNumero);
+
+subtracao(primeiroNumero, segundoNumero);
+
+multiplicacao(primeiroNumero, segundoNumero);
+
+divisao(primeiroNumero, segundoNumero);
+
